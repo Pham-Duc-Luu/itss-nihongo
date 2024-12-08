5cc2065f7fdc2a3e96d6dbd63ac209b7b1861dea
Quang <quang.dd210716@sis.hust.edu.vn>
2024-12-08 21:19:34 +0700

    ui-logon

--- app/[local]/profile/layout.tsx
+++ app/[local]/profile/layout.tsx
@@ -1,39 +1,19 @@
-// app/layout.tsx
-export const metadata = {
-    title: 'User Profile',
-  };
-  
-  export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-      <html lang="en">
-        <body
-          style={{
-            margin: 0,
-            fontFamily: 'Arial, sans-serif',
-            backgroundColor: '#EAFBF7',
-          }}
-        >
-          {/* Header */}
-          <header
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              padding: '10px 20px',
-              backgroundColor: '#ffffff',
-              borderBottom: '1px solid #ddd',
-            }}
-          >
-            <h1 style={{ fontSize: '20px', margin: 0 }}>Logo</h1>
-            <img
-              src="https://via.placeholder.com/40"
-              alt="User avatar"
-              style={{ width: '40px', height: '40px', borderRadius: '50%' }}
-            />
-          </header>
-          {children}
-        </body>
-      </html>
-    );
-  }
-  
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/Sidebar";
+import { Divider } from "@nextui-org/react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className=" min-h-screen flex flex-col">
+      <Navbar></Navbar>
+      <div className="flex flex-1">
+        <Sidebar></Sidebar>
+        {children}
+      </div>
+    </div>
+  );
+}
